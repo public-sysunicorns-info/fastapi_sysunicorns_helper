f8750e9771e88102aec6fc0f8dc99a43b1e96c89
Victorien VANROYE <victorien.vanroye@gmail.com>
2022-02-15 08:45:30 +0100

    fix(release): fix settings of semantic-release

--- script/release-github-workflow.sh
+++ script/release-github-workflow.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-plugin="@semantic-release/github"
-plugin="${plugin},@semantic-release/commit-analyzer"
-plugin="${plugin},@semantic-release/release-notes-generator"
-plugin="${plugin},@semantic-release/changelog"
-plugin="${plugin},@semantic-release/exec"
-plugin="${plugin},@semantic-release/git"
-
-npx semantic-release -b main -p "${plugin}"
+npx semantic-release -b main
